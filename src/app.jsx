c63dcb05c18b14cbb3582ab4a406829e882ec39c
okpiankov <sibiryak86@yandex.ru>
2024-06-17 18:14:38 +0300

    подключил redux во всем проекте

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,12 @@
 import { Provider } from 'react-redux';
+import { rootStore } from './store/store';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
-// import { rootStore } from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, rootStore } from './store/store';
 
 export const App = () => {
   return (
     <Provider store={rootStore}>
-      <PersistGate loading={null} persistor={persistor}>
-        <RouterProvider router={router} />;
-      </PersistGate>
+      <RouterProvider router={router} />;
     </Provider>
   );
 };
